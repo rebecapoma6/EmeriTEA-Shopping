2b2ebd19454cc771ec48df10636e94e141d7ade0
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-10-24 12:12:19 +0200

    add main

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+import './index.css'
+import Home from './Componets/Home/Home.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+    <Home/>
+
+
+  </React.StrictMode>,
+)
---
+++ src/views/Subscriber/Subscriber.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+
+
+const Subscriber = () => {
+
+  return (
+    <>
+        <Home />
+    </>
+  );
+}
+
+export default Subscriber;
