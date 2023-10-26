dfb83e5b57610f4bc768139fca5fc7a3689a6900
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-10-26 13:07:57 +0200

    Login ok, entra a admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './views/Login/Login';
+import Admin from './views/Admin/Admin';
 
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
 
-import './index.css'
-import Home from './Componets/Home/Home.jsx'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+reactRoot.render(
   <React.StrictMode>
-  
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
-
-  </React.StrictMode>,
-)
---
+++ src/views/Admin/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hello = () => {
+
+    return (
+        <>
+            <div>
+                <h1>Welcome Admin</h1>
+            </div>
+        </>
+    );
+}
+
+export default Hello;
