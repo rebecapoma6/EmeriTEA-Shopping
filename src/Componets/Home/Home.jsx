5651d13d96afd8646f7882081cb02b4fa11ad205
Javiser <jascjav@gmail.com>
2023-10-30 19:25:08 +0100

    cambios

--- src/Componets/Home/Home.jsx
+++ src/Componets/Home/Home.jsx
@@ -1,18 +1,80 @@
-import React from 'react';
-import Header from '../Header/header';
-import './Home.css';
+import React from "react";
+import Header from "../Header/header";
+import "./Home.css";
+import CustomNavbar from "../Navbar/Navbar";
+import CarouselSection from "../Clothing/Clothing";
 
 const Home = () => {
   return (
     <>
-    <div className= "home" >
-    <Header/>
-    
-      <h1>Hola productos</h1>
-    </div>
+      <div className="homeinitial">
+        <Header />
+
+        <CustomNavbar />
+        <div className="intro">
+          <h2>
+            Esta es una tienda solidaria que a sido creada por la asociacion
+            EmeriTEA, con fin de ayudar a las familias con personas con TEA.
+          </h2>
+        </div>
+        
+
+        
+        
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+        <br />        <br />
+             
+         </div>
     </>
   );
 };
 
 export default Home;
-
--- src/Componets/Navbar/Navbar.jsx
+++ src/Componets/Navbar/Navbar.jsx
@@ -1,4 +1,53 @@
 
+import React from 'react';
+import './Navbar.css'; 
+
+const CustomNavbar = () => {
+  return (
+    <div className="mainnavbar">
+    <div className="navbar">
+      <a href="#quienes-somos">Quiénes Somos</a>
+      <a href="#donaciones">Donaciones</a>
+      <div className="dropdown">
+        <a href="#productos" className="dropbtn">Productos</a>
+        {/* <button className="dropbtn">Productos</button> */}
+        <div className="dropdown-content">
+          <a href="#categoria-camisetas">Camisetas</a>
+          <a href="#categoria-accesorios">Accesorios</a>
+        </div>
+      </div>
+      <a href="#eventos">Eventos</a>
+      <a href="#ubicacion">Ubicación</a>
+    </div>
+    </div>
+  );
+};
+
+export default CustomNavbar;
 
 
 
+
+
+// import React from 'react';
+// import './Navbar.css'; 
+
+// const CustomNavbar = () => {
+//   return (
+//     <div className="navbar">
+//       <a href="#quienes-somos">Quiénes Somos</a>
+//       <a href="#donaciones">Donaciones</a>
+//       <div className="dropdown">
+//         <a href="#productos" className="dropbtn">Productos</a>
+//         <div className="dropdown-content">
+//           <a href="#categoria-camisetas">Camisetas</a>
+//           <a href="#categoria-accesorios">Accesorios</a>
+//         </div>
+//       </div>
+//       <a href="#eventos">Eventos</a>
+//       <a href="#ubicacion">Ubicación</a>
+//     </div>
+//   );
+// };
+
+// export default CustomNavbar;
