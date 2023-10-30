ae7fe3a614357d417d4eac2ee16a72ab2fad320f
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-10-30 12:16:14 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// // import {NextUIProvider} from '@nextui-org/react'
-// import Login from './views/Login/Login';
-// import Admin from './views/Admin/Admin';
-
-// const root = document.getElementById('root');
-// const reactRoot = createRoot(root);
-
-// reactRoot.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />}
-//        <Route path="/login" element={<Login />} />
-//       <Route path="/admin" element={<Admin />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
