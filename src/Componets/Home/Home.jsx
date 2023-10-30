1ca36203c3a6c07dab5fc505bbece38747979f6c
Javiser <jascjav@gmail.com>
2023-10-30 12:56:41 +0100

    header

--- src/Componets/Home/Home.jsx
+++ src/Componets/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Header from '../Header/header';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <>
+    <div className= "home" >
+    <Header/>
+    
       <h1>Hola productos</h1>
     </div>
+    </>
   );
 };
 
 export default Home;
+
