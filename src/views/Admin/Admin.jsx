bbc0b28c75302f896742743a00184b5cc6237308
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-11-01 12:56:36 +0100

    add

--- src/views/Admin/Admin.jsx
+++ src/views/Admin/Admin.jsx
@@ -157,78 +157,82 @@ const Admin = () => {
 
     return (
         <>
-            <h1>HOLA ADMIN</h1>
-            <br />
-            <Button className='modal-header' onClick={openAddModal}>Add Product</Button>
+        <h1>HOLA ADMIN</h1>
+        <br />
+        <Button className='modal-header' onClick={openAddModal}>Add Product</Button>
 
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false}>
-                <ModalContent className='jarra'>
-                    <>
-                        <ModalHeader className="flex flex-col gap-1">Add Product</ModalHeader>
-                        <ModalBody className="modal-form">
-                            <form>
-                                <label htmlFor="productName">Name:</label>
-                                <input
-                                    type="text"
-                                    id="productName"
-                                    value={newProduct.name}
-                                    onChange={(e) => handleInputChange('name', e.target.value)}
-                                />
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false}>
+            <ModalContent className='jarra'>
+                <>
+                    <ModalHeader className="flex flex-col gap-1">Add Product</ModalHeader>
+                    <ModalBody className="modal-form">
+                        <form>
+                            <label htmlFor="productName">Name:</label>
+                            <input
+                                type="text"
+                                id="productName"
+                                value={newProduct.name}
+                                onChange={(e) => handleInputChange('name', e.target.value)}
+                            />
 
-                                <label htmlFor="productCategory">Category:</label>
-                                <input
-                                    type="text"
-                                    id="productCategory"
-                                    value={newProduct.category}
-                                    onChange={(e) => handleInputChange('category', e.target.value)}
-                                />
+                            <label htmlFor="productCategory">Category:</label>
+                            <select
+                                id="productCategory"
+                                value={newProduct.category}
+                                onChange={(e) => handleInputChange('category', e.target.value)}
+                            >
+                                <option value="">Select a category</option>
+                                <option value="Accessories">Accessories</option>
+                                <option value="Clothing">Clothing</option>
+                            </select>
+                            <br />
 
-                                <label htmlFor="productPrice">Price:</label>
-                                <input
-                                    type="text"
-                                    id="productPrice"
-                                    value={newProduct.price}
-                                    onChange={(e) => handleInputChange('price', e.target.value)}
-                                />
+                            <label htmlFor="productPrice">Price:</label>
+                            <input
+                                type="text"
+                                id="productPrice"
+                                value={newProduct.price}
+                                onChange={(e) => handleInputChange('price', e.target.value)}
+                            />
 
-                                {newProduct.image && (
-                                    <img src={newProduct.image} alt="Product Image" className="product-image" />
-                                )}
-                                <label className="product-image" htmlFor="productImage">Image URL:</label>
-                                <input
-                                    type="text"
-                                    id="productImage"
-                                    value={newProduct.image}
-                                    onChange={(e) => handleInputChange('image', e.target.value)}
-                                />
+                            {newProduct.image && (
+                                <img src={newProduct.image} alt="Product Image" className="product-image" />
+                            )}
+                            <label className="product-image" htmlFor="productImage">Image URL:</label>
+                            <input
+                                type="text"
+                                id="productImage"
+                                value={newProduct.image}
+                                onChange={(e) => handleInputChange('image', e.target.value)}
+                            />
 
-                                <label htmlFor="productDescription">Description:</label>
-                                <textarea
-                                    id="productDescription"
-                                    value={newProduct.description}
-                                    onChange={(e) => handleInputChange('description', e.target.value)}
-                                ></textarea>
+                            <label htmlFor="productDescription">Description:</label>
+                            <textarea
+                                id="productDescription"
+                                value={newProduct.description}
+                                onChange={(e) => handleInputChange('description', e.target.value)}
+                            ></textarea>
 
-                                <label htmlFor="productStock">Stock:</label>
-                                <input
-                                    type="text"
-                                    id="productStock"
-                                    value={newProduct.stock}
-                                    onChange={(e) => handleInputChange('stock', e.target.value)}
-                                />
-                            </form>
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button color="danger" variant="light" onClick={closeAddModal}>
-                                Close
-                            </Button>
-                            <Button color="primary" onClick={addProduct}>
-                                Add
-                            </Button>
-                        </ModalFooter>
-                    </>
-                </ModalContent>
-            </Modal>
+                            <label htmlFor="productStock">Stock:</label>
+                            <input
+                                type="text"
+                                id="productStock"
+                                value={newProduct.stock}
+                                onChange={(e) => handleInputChange('stock', e.target.value)}
+                            />
+                        </form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" variant="light" onClick={closeAddModal}>
+                            Close
+                        </Button>
+                        <Button color="primary" onClick={addProduct}>
+                            Add
+                        </Button>
+                    </ModalFooter>
+                </>
+            </ModalContent>
+        </Modal>
 
             <div className="product-list">
                 {products.map((product) => (
@@ -257,13 +261,18 @@ const Admin = () => {
                                     value={selectedProductDetails.name}
                                     onChange={(e) => setSelectedProductDetails({ ...selectedProductDetails, name: e.target.value })}
                                 />
-                                {/* Agrega el resto de los campos para editar */}
+
                                 <label htmlFor="productCategory">Category:</label>
-                                <input
-                                    type="text"
+                                <select
                                     value={selectedProductDetails.category}
                                     onChange={(e) => setSelectedProductDetails({ ...selectedProductDetails, category: e.target.value })}
-                                />
+                                >
+                                    <option value="">Select a </option>
+                                    <option value="Accessories">Accessories</option>
+                                    <option value="Clothing">Clothing</option>
+                                </select>
+                                <br>
+                                </br>
 
                                 <label htmlFor="productPrice">Price:</label>
                                 <input
