2049a4a8bd842718dfc1025292de83b5614e0583
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-11-03 17:24:59 +0100

    add css admin

--- src/Componets/Card/Card.jsx
+++ src/Componets/Card/Card.jsx
@@ -1,43 +1,47 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import { Stack, Button } from '@mui/material'; // Importa Stack y Button desde Material-UI
 import Typography from '@mui/material/Typography';
+import "./Card.css";
 
 const ProductCard = ({ product, deleteProduct, openEditModal }) => {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt={product.Name_product}
-        height="150"
-        image={product.image}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {product.Name_product}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Category: {product.category}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Price: {product.price} €
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Description: {product.description}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Stock: {product.stock}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button onClick={() => deleteProduct(product.id)}>Delete</Button>
-        <Button onClick={() => openEditModal(product)}>Edit</Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                alt={product.Name_product}
+                height="340"               
+                image={product.image}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {product.Name_product}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Category: {product.category}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Price: {product.price} €
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Description: {product.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Stock: {product.stock}
+                </Typography>
+
+                <Stack spacing={2} direction="row"  >
+
+                    <Button onClick={() => deleteProduct(product.id)} variant="contained">Delete</Button>
+                    <Button onClick={() => openEditModal(product)} variant="contained">Edit</Button>
+
+                </Stack>
+            </CardContent>
+        </Card>
+
+    );
 };
 
 export default ProductCard;
