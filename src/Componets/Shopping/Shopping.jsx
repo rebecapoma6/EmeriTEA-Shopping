cdc358b86c2a147931dd0fef18e795d2d1c47af8
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-11-23 12:53:09 +0100

    add

--- src/Componets/Shopping/Shopping.jsx
+++ src/Componets/Shopping/Shopping.jsx
@@ -1,102 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-// import './Shopping.css';
-
-// const ShoppingCart = ({ cart, removeFromCart }) => {
-//   const [productQuantities, setProductQuantities] = useState(
-//     cart.map(() => 1) // Inicializa todas las cantidades en 1 por defecto
-//   );
-
-//   const [totalPrice, setTotalPrice] = useState(0);
-
-//   useEffect(() => {
-//     // Calcula la suma total de los precios teniendo en cuenta la cantidad de cada producto
-//     const total = cart.reduce((sum, product, index) => {
-//       return sum + product.price * productQuantities[index];
-//     }, 0);
-//     setTotalPrice(total);
-//   }, [cart, productQuantities]);
-
-//   const removeItemFromCart = (productId) => {
-//     // Usa removeFromCart para eliminar un producto del carrito
-//     removeFromCart(productId);
-
-//     // Actualiza las cantidades y el total después de eliminar un producto
-//     setProductQuantities((prevQuantities) => {
-//       const newQuantities = [...prevQuantities];
-//       newQuantities.pop(); // Elimina la última cantidad porque eliminamos un producto
-//       return newQuantities;
-//     });
-//   };
-
-//   const incrementQuantity = () => {
-//     setProductQuantities((prevQuantities) => [...prevQuantities, 1]);
-//   };
-
-//   const decrementQuantity = () => {
-//     setProductQuantities((prevQuantities) => {
-//       if (prevQuantities.length > 1) {
-//         const newQuantities = [...prevQuantities];
-//         newQuantities.pop(); // Elimina la última cantidad
-//         return newQuantities;
-//       } else {
-//         // No permitas que la cantidad sea menor que 1
-//         return prevQuantities;
-//       }
-//     });
-//   };
-
-//   return (
-//     <div className="shopping-cart">
-//       <h2>Carrito de Compras</h2>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Imagen</th>
-//             <th>Nombre del Producto</th>
-//             <th>Precio</th>
-//             <th>Talla</th>
-//             <th>Cantidad</th>
-//             <th>Subtotal</th>
-//             <th>Eliminar</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {cart.map((product, index) => (
-//             <tr key={`${product.id_Product}-${index}`}>
-//               <td><img className="product-imagecart" src={product.image} alt={product.name_product} /></td>
-//               <td>{product.name_product}</td>
-//               <td>{product.price} €</td>
-//               <td>{product.size}</td>
-//               <td>
-//                 <button onClick={() => decrementQuantity()}>-</button>
-//                 {productQuantities[index]}
-//                 <button onClick={() => incrementQuantity()}>+</button>
-//               </td>
-//               <td>{product.price * productQuantities[index]} €</td>
-//               <td>
-//                 <button className="remove-button" onClick={() => removeItemFromCart(product.id_Product)}>
-//                   <FontAwesomeIcon icon={faTrashAlt} />
-//                 </button>
-//               </td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <p>Total: {totalPrice} €</p>
-//     </div>
-//   );
-// };
-
-// export default ShoppingCart;
-
-
-
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
@@ -123,21 +24,40 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
 
     // Actualiza las cantidades y el total después de eliminar un producto
     setProductQuantities((prevQuantities) => {
+      const index = cart.findIndex(product => product.id_Product === productId);
+      if (index !== -1) {
+        const newQuantities = [...prevQuantities];
+        newQuantities.splice(index, 1); // Elimina la cantidad correspondiente al producto eliminado
+        return newQuantities;
+      } else {
+        return prevQuantities;
+      }
+    });
+
+    // Actualiza también el total restando el precio del producto eliminado
+    setTotalPrice((prevTotal) => {
+      const productToRemove = cart.find(product => product.id_Product === productId);
+      if (productToRemove) {
+        return prevTotal - productToRemove.price * productQuantities[index];
+      } else {
+        return prevTotal;
+      }
+    });
+  };
+
+  const incrementQuantity = (index) => {
+    setProductQuantities((prevQuantities) => {
       const newQuantities = [...prevQuantities];
-      newQuantities.pop(); // Elimina la última cantidad porque eliminamos un producto
+      newQuantities[index]++;
       return newQuantities;
     });
   };
 
-  const incrementQuantity = () => {
-    setProductQuantities((prevQuantities) => [...prevQuantities, 1]);
-  };
-
-  const decrementQuantity = () => {
+  const decrementQuantity = (index) => {
     setProductQuantities((prevQuantities) => {
-      if (prevQuantities.length > 1) {
+      if (prevQuantities[index] > 1) {
         const newQuantities = [...prevQuantities];
-        newQuantities.pop(); // Elimina la última cantidad
+        newQuantities[index]--;
         return newQuantities;
       } else {
         // No permitas que la cantidad sea menor que 1
@@ -174,9 +94,9 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
               <td>{product.price} €</td>
               <td>{product.size}</td>
               <td>
-                <button onClick={decrementQuantity}>-</button>
+                <button onClick={() => decrementQuantity(index)}>-</button>
                 {productQuantities[index]}
-                <button onClick={incrementQuantity}>+</button>
+                <button onClick={() => incrementQuantity(index)}>+</button>
               </td>
               <td>{product.price * productQuantities[index]} €</td>
               <td>
@@ -199,9 +119,6 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
 };
 
 export default ShoppingCart;
-
-
-
 
 
 
