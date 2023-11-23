9d28b45302c60d1c3ff4dad1b69773860a44c6b1
Anthony Brayan <brayan.farronan@gmail.com>
2023-11-23 12:57:10 +0100

    .....

--- src/Componets/Shopping/Shopping.jsx
+++ src/Componets/Shopping/Shopping.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
@@ -24,40 +26,21 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
 
     // Actualiza las cantidades y el total después de eliminar un producto
     setProductQuantities((prevQuantities) => {
-      const index = cart.findIndex(product => product.id_Product === productId);
-      if (index !== -1) {
-        const newQuantities = [...prevQuantities];
-        newQuantities.splice(index, 1); // Elimina la cantidad correspondiente al producto eliminado
-        return newQuantities;
-      } else {
-        return prevQuantities;
-      }
-    });
-
-    // Actualiza también el total restando el precio del producto eliminado
-    setTotalPrice((prevTotal) => {
-      const productToRemove = cart.find(product => product.id_Product === productId);
-      if (productToRemove) {
-        return prevTotal - productToRemove.price * productQuantities[index];
-      } else {
-        return prevTotal;
-      }
-    });
-  };
-
-  const incrementQuantity = (index) => {
-    setProductQuantities((prevQuantities) => {
       const newQuantities = [...prevQuantities];
-      newQuantities[index]++;
+      newQuantities.pop(); // Elimina la última cantidad porque eliminamos un producto
       return newQuantities;
     });
   };
 
-  const decrementQuantity = (index) => {
+  const incrementQuantity = () => {
+    setProductQuantities((prevQuantities) => [...prevQuantities, 1]);
+  };
+
+  const decrementQuantity = () => {
     setProductQuantities((prevQuantities) => {
-      if (prevQuantities[index] > 1) {
+      if (prevQuantities.length > 1) {
         const newQuantities = [...prevQuantities];
-        newQuantities[index]--;
+        newQuantities.pop(); // Elimina la última cantidad
         return newQuantities;
       } else {
         // No permitas que la cantidad sea menor que 1
@@ -94,9 +77,9 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
               <td>{product.price} €</td>
               <td>{product.size}</td>
               <td>
-                <button onClick={() => decrementQuantity(index)}>-</button>
+                <button onClick={decrementQuantity}>-</button>
                 {productQuantities[index]}
-                <button onClick={() => incrementQuantity(index)}>+</button>
+                <button onClick={incrementQuantity}>+</button>
               </td>
               <td>{product.price * productQuantities[index]} €</td>
               <td>
@@ -109,7 +92,7 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
         </tbody>
       </table>
       <div className="checkout-container">
-        <p>Total: {totalPrice} €</p>
+        <p className="pTotal">Total: {totalPrice} €</p>
         <button className="buy-button" onClick={handleBuy}>
           Comprar
         </button>
@@ -120,111 +103,6 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
 
 export default ShoppingCart;
 
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-// import './Shopping.css';
-
-// const ShoppingCart = ({ cart, removeFromCart, updateQuantity }) => {
-//   const [cartData, setCartData] = useState([]);
-
-//   useEffect(() => {
-//     fetchCartData();
-//   }, [cart]);
-
-//   const fetchCartData = async () => {
-//     try {
-//       const response = await fetch('URL_DEL_BACKEND/api/cart'); // Reemplaza 'URL_DEL_BACKEND' con la URL real de tu backend
-//       if (!response.ok) {
-//         throw new Error('Error al obtener datos del carrito');
-//       }
-//       const data = await response.json();
-//       setCartData(data);
-//     } catch (error) {
-//       console.error('Error al obtener datos del carrito:', error);
-//     }
-//   };
-
-//   const saveCartData = async (newCartData) => {
-//     try {
-//       const response = await fetch('URL_DEL_BACKEND/api/cart', { // Reemplaza 'URL_DEL_BACKEND' con la URL real de tu backend
-//         method: 'PUT',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(newCartData),
-//       });
-
-//       if (!response.ok) {
-//         throw new Error('Error al guardar datos del carrito');
-//       }
-//     } catch (error) {
-//       console.error('Error al guardar datos del carrito:', error);
-//     }
-//   };
-
-//   const calculateSubtotal = (product) => {
-//     return product.Price_product * product.Quantity_product;
-//   };
-
-//   const calculateTotal = () => {
-//     return cartData.reduce((total, product) => total + calculateSubtotal(product), 0);
-//   };
-
-//   const saveCartOnChange = () => {
-//     saveCartData(cartData);
-//   };
-
-//   useEffect(() => {
-//     saveCartOnChange();
-//   }, [cartData]);
-
-//   return (
-//     <div className="shopping-cart">
-//       <h2>Carrito de Compras</h2>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Imagen</th>
-//             <th>Nombre del Producto</th>
-//             <th>Precio</th>
-//             <th>Talla</th>
-//             <th>Cantidad</th>
-//             <th>Subtotal</th>
-//             <th>Eliminar</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {cartData.map((product) => (
-//             <tr key={product.Id_guestCart}>
-//               <td><img className="product-imagecart" src={product.Product.image} alt={product.Product.name_product} /></td>
-//               <td>{product.Product.name_product}</td>
-//               <td>{product.Price_product} €</td>
-//               <td>{product.Product.size}</td>
-//               <td>
-//                 <button onClick={() => updateQuantity(product.id_Product, product.Quantity_product - 1)}>-</button>
-//                 {product.Quantity_product}
-//                 <button onClick={() => updateQuantity(product.id_Product, product.Quantity_product + 1)}>+</button>
-//               </td>
-//               <td>{calculateSubtotal(product)} €</td>
-//               <td>
-//                 <button className="remove-button" onClick={() => removeFromCart(product.id_Product)}>
-//                   <FontAwesomeIcon icon={faTrashAlt} />
-//                 </button>
-//               </td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <p>Total: {calculateTotal()} €</p>
-//     </div>
-//   );
-// };
-
-// export default ShoppingCart;
 
 
 
