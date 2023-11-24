0efc9cc4162588cf3a4fdecacd6f68689a5b7c40
JaviSeC <jascjav@gmail.com>
2023-11-24 15:49:39 +0100

    sdvasf

--- src/Componets/Header/Header.test.jsx
+++ src/Componets/Header/Header.test.jsx
@@ -1,29 +1,20 @@
-
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
 describe('Header', () => {
- test('renders Header component', () => {
+ it('should render the header with the correct links and buttons', () => {
  render(
- <Router>
- <Header />
- </Router>
+  <Router>
+    <Header />
+  </Router>
  );
- const titleElement = screen.getByText(/EmeriTEA Market/i);
- expect(titleElement).toBeInTheDocument();
- });
 
- test('renders button correctly', () => {
- render(
- <Router>
- <Header />
- </Router>
- );
- const buttonElement = screen.getByRole('button');
- expect(buttonElement).toBeInTheDocument();
+ expect(screen.getByAltText('LogoEmeriatea')).toBeInTheDocument();
+ expect(screen.getByText('EmeriTEA Market')).toBeInTheDocument();
+ expect(screen.getByRole('button')).toBeInTheDocument();
+ expect(screen.getAllByRole('link')[1]).toBeInTheDocument();
+
  });
 });
-
-
--- src/Componets/Solidary/Solidary.test.jsx
+++ src/Componets/Solidary/Solidary.test.jsx
@@ -1,29 +1,58 @@
+// import { render, screen } from '@testing-library/react';
+// import { BrowserRouter } from 'react-router-dom';
+// import Solidary from './Solidary';
+// import '@testing-library/jest-dom';
+
+
+// describe('Solidary', () => {
+//  test('renders Solidary component', () => {
+//   render(
+//     <BrowserRouter>
+//       <Solidary />
+//     </BrowserRouter>
+//   );
+//   const titleElement = screen.getByText(/Haz Tú Regalo Solidario!/i);
+//   expect(titleElement).toBeInTheDocument();
+//  });
+
+//  test('renders images correctly', () => {
+//    render(<Solidary />);
+//    const imageElements = screen.getAllByRole('img');
+//    expect(imageElements).toHaveLength(4);
+//  });
+
+//  test('renders text correctly', () => {
+//    render(<Solidary />);
+//    const textElement = screen.getByText(/Haz tú regalo personalizado para eventos, fiestas, bodas, cumpleaños, etc...!/i);
+//    expect(textElement).toBeInTheDocument();
+//  });
+
+//  test('renders button correctly', () => {
+//    render(<Solidary />);
+//    const buttonElement = screen.getByRole('button', { name: /Más información aquí!/i });
+//    expect(buttonElement).toBeInTheDocument();
+//  });
+// });
+
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import { BrowserRouter } from 'react-router-dom';
 import Solidary from './Solidary';
+import '@testing-library/jest-dom';
 
 describe('Solidary', () => {
  test('renders Solidary component', () => {
-  render(<Solidary />);
-  const titleElement = screen.getByText(/Haz tu Regalo Solidario!/i);
-  expect(titleElement).toBeInTheDocument();
+ render(
+  <BrowserRouter>
+    <Solidary />
+  </BrowserRouter>
+ );
+ const titleElement = screen.getByText(/Haz Tú Regalo Solidario!/i);
+ expect(titleElement).toBeInTheDocument();
  });
 
- test('renders images correctly', () => {
-  render(<Solidary />);
-  const imageElements = screen.getAllByRole('img');
-  expect(imageElements).toHaveLength(4);
- });
+ 
+ 
 
- test('renders text correctly', () => {
-  render(<Solidary />);
-  const textElement = screen.getByText(/Haz tu regalo personalizado para eventos, fiestas, bodas, cumpleaños, etc...!/i);
-  expect(textElement).toBeInTheDocument();
- });
-
- test('renders button correctly', () => {
-  render(<Solidary />);
-  const buttonElement = screen.getByRole('button', { name: /Más información aquí!/i });
-  expect(buttonElement).toBeInTheDocument();
- });
+ // ...
 });
+
